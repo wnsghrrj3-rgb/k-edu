5f38b24b36496f4b2af0e125e8bc416ce610e628
kedu <k@edu>
2026-09-09 01:31:24 +0000

    science: 4-2 ①단원 l03 「여러 날 동안 변하는 달의 모양」 배포 (3/8) + kedu_map 갱신

--- kedu_map/_index.js
+++ kedu_map/_index.js
@@ -88,7 +88,7 @@ window.KEDU_MAP_INDEX = [
   "key": "g4_2_science",
   "units": 1,
   "lessons": 8,
-  "ready": 2
+  "ready": 3
  },
  {
   "key": "g5_1_korean",
--- kedu_map/g4_2_science.js
+++ kedu_map/g4_2_science.js
@@ -34,14 +34,14 @@ window.KEDU_MAP["g4_2_science"] = {
      "ready": true
     },
     {
-     "key": null,
-     "lessonId": null,
+     "key": "g4_science_u1_l03",
+     "lessonId": "g4_s2_sci_u1_l03_v1",
      "n": "3",
      "title": "여러 날 동안 변하는 달의 모양",
      "sub": null,
-     "url": null,
-     "track": "soon",
-     "ready": false
+     "url": "/grade4/semester2/science/1단원_밤하늘관찰/g4_sci_u1_l03_변하는달의모양.html",
+     "track": "tb",
+     "ready": true
     },
     {
      "key": null,
